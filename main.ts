a33fe67cf6b0b191038339eaf1259a867cde9a8e
nick747 <84738664+nick747@users.noreply.github.com>
2023-08-18 17:08:28 +0200

    fix: improved changeColor function

--- main.ts
+++ main.ts
@@ -10,51 +10,22 @@ function generateColor(): string {
 }
 
 
+
 function changeColorText(): void {
-    let color1 = document.getElementById('color1') as HTMLElement
-    let color1Hex = document.getElementById('color1Hex');
+    let colorContainers = document.querySelectorAll('colorContainer') as NodeListOf<HTMLElement>;
+    let colorHexs = document.querySelectorAll('colorHex');
 
-    if (color1) {
-        color1.style.backgroundColor = `#${generateColor()}`;
-    }
+    colorContainers.forEach((container) => {
+        if (container) {
+            container.style.backgroundColor = `#${generateColor()}`;
+        }
+    })
 
-    if (color1Hex) {
-        color1Hex.innerHTML = `#${generateColor()}`;
-    }    
-
-    let color2 = document.getElementById('color2') as HTMLElement
-    let color2Hex = document.getElementById('color2Hex');
-
-    if (color2) {
-        color2.style.backgroundColor = `#${generateColor()}`;
-    }
-
-    if (color2Hex) {
-        color2Hex.innerHTML = `#${generateColor()}`;
-    }
-
-    let color3 = document.getElementById('color3') as HTMLElement
-    let color3Hex = document.getElementById('color3Hex');
-
-    if (color3) {
-        color3.style.backgroundColor = `#${generateColor()}`;
-    }
-
-    if (color3Hex) {
-        color3Hex.innerHTML = `#${generateColor()}`;
-    }
-
-    let color4 = document.getElementById('color4') as HTMLElement
-    let color4Hex = document.getElementById('color4Hex');
-
-    if (color4) {
-        color4.style.backgroundColor = `#${generateColor()}`;
-    }
-
-    if (color4Hex) {
-        color4Hex.innerHTML = `#${generateColor()}`;
-    }
-    
+    colorHexs .forEach((hex) => {
+        if (hex) {
+            hex.innerHTML = `#${generateColor()}`;
+        }
+    })
 }
 
 function button() {
